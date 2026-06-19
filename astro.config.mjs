26afa8e7be0631598dbf61e6be9c755b1ceb9857
Billy Gunawan <billgun.dev@gmail.com>
2026-06-19 18:32:28 +0700

    Remove Cloudflare adapter, restore static output for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://billgun.github.io',
-  adapter: cloudflare(),
+  site: "https://billgun.github.io",
 });
